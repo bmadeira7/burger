1c4ebfb17d39cca992d3cea75e0f828ebf41a76c
thisisbrix901 <bmadeira7@gmail.com>
2018-10-20 21:01:45 -0400

    this hw sucks

--- server.js
+++ server.js
@@ -18,59 +18,92 @@ app.set("view engine", "handlebars");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
 
-  console.log("connected to mysql as: id " + connection.threadId);
+    console.log("connected to mysql as: id " + connection.threadId);
 });
 
 
 // Use Handlebars to render the main index.html page with the todos in it.
-app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burgers;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
-  
-      res.render("index", { burgers: data });
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
+        //this res.render is responsible for making the data display in the html page
+        res.render("index", { burgers: data });
+     
+        console.log("burger at index 0: " +data[0].burger_name)
     });
-  });
-  // Create a new todo
-  app.post('/todos', function(req, res){
-    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.plan], function(err, result){
+});
+
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
+        // Handlebars requires an object to be sent to the index.handlebars file.
+        var eatenBurger = [];
+        for (i = 0; i < burgers.length; i++) {
+            if (burgers[i].devoured) {
+                (eatenBurger).push(burgers[i])
+            }
+        }
+        console.log("eatenBurg: " + eatenBurger)
+        // 3. Send all the animals that are not pets to the index.handlebars file.
+        res.render("index", { burgers: eatenBurger });
+
+    });
+})
+// Update a todo
+app.put('/todos/:id', function(req, res){
+    connection.query("UPDATE burgers SET devoured = ? WHERE id =?", [req.body.devoured ,req.params.id], function(err, result){
       if(err){
         return res.status(500).end();
-      } 
-      res.json({id: result.insertId})
-      console.log({id: result.insertId})
-  
+      }
+      else if(result.affectedRows === 0){
+        return res.status(404).end();
+      }
+      res.status(200).end();
     })
   })
-  
-// Delete 
-  app.delete('/todos/:id', function(req, res){
-    connection.query("DELETE FROM burgers WHERE id =?", [req.params.id], function(err, result){
-        console.log(req.params.id)
-       if(err){
-         return res.status(500).end();
-       }
-       else if(result.affectedRows === 0){
-         return res.status(404).end();
-       }
-       res.status(200).end();
+    // Create a new burger
+    app.post('/burgers', function (req, res) {
+        connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.plan], function (err, result) {
+            if (err) {
+                return res.status(500).end();
+            }
+            res.json({ id: result.insertId })
+            // console.log({ id: result.insertId })
+            // console.log(req.body.plan)
+
+        })
     })
-  })
-  
+
+    // Delete 
+    app.delete('/burgers/:id', function (req, res) {
+        connection.query("DELETE FROM burgers WHERE id =?", [req.params.id], function (err, result) {
+            console.log("deleted burger with id of: " + req.params.id)
+            if (err) {
+                return res.status(500).end();
+            }
+            else if (result.affectedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        })
+    })
 
 
 
@@ -83,8 +116,8 @@ app.get("/", function(req, res) {
 
 
 
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+
+    app.listen(PORT, function () {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+    });

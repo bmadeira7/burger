ba859efb95e002e8556cfe57f0e59a6639b12016
thisisbrix901 <bmadeira7@gmail.com>
2018-10-21 21:00:26 -0400

    heroku jaws setup

--- server.js
+++ server.js
@@ -15,16 +15,19 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+var connection;
 
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -43,8 +46,8 @@ app.get("/", function (req, res) {
         }
         //this res.render is responsible for making the data display in the html page
         res.render("index", { burgers: data });
-     
-        console.log("burger at index 0: " +data[0].burger_name)
+
+        console.log("burger at index 0: " + data[0].burger_name)
     });
 });
 
@@ -61,50 +64,50 @@ app.get("/", function (req, res) {
             }
         }
         console.log("eatenBurg: " + eatenBurger)
-      
+
         res.render("index", { burgers: eatenBurger });
 
     });
 })
 // Update a burger
-app.put('/burgers/:id', function(req, res){
+app.put('/burgers/:id', function (req, res) {
     console.log("HEYHEYHEYHEYHEY")
-    connection.query("UPDATE burgers SET ? WHERE id=?", [{devoured: true}, req.params.id], function(err, result){
-      if(err){
-        return res.status(500).end();
-      }
-      else if(result.affectedRows === 0){
-        return res.status(404).end();
-      }
-      res.status(200).end();
+    connection.query("UPDATE burgers SET ? WHERE id=?", [{ devoured: true }, req.params.id], function (err, result) {
+        if (err) {
+            return res.status(500).end();
+        }
+        else if (result.affectedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     })
-  })
-    // Create a new burger
-    app.post('/burgers', function (req, res) {
-        connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger], function (err, result) {
-            if (err) {
-                return res.status(500).end();
-            }
-            res.json({ id: result.insertId })
-            // console.log({ id: result.insertId })
-            // console.log(req.body.plan)
+})
+// Create a new burger
+app.post('/burgers', function (req, res) {
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger], function (err, result) {
+        if (err) {
+            return res.status(500).end();
+        }
+        res.json({ id: result.insertId })
+        // console.log({ id: result.insertId })
+        // console.log(req.body.plan)
 
-        })
     })
+})
 
-    // Delete 
-    app.delete('/burgers/:id', function (req, res) {
-        connection.query("DELETE FROM burgers WHERE id =?", [req.params.id], function (err, result) {
-            console.log("deleted burger with id of: " + req.params.id)
-            if (err) {
-                return res.status(500).end();
-            }
-            else if (result.affectedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        })
+// Delete 
+app.delete('/burgers/:id', function (req, res) {
+    connection.query("DELETE FROM burgers WHERE id =?", [req.params.id], function (err, result) {
+        console.log("deleted burger with id of: " + req.params.id)
+        if (err) {
+            return res.status(500).end();
+        }
+        else if (result.affectedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     })
+})
 
 
 
@@ -118,7 +121,7 @@ app.put('/burgers/:id', function(req, res){
 
 
 
-    app.listen(PORT, function () {
-        // Log (server-side) when our server has started
-        console.log("Server listening on: http://localhost:" + PORT);
-    });
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
